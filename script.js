3da7a87caba746928f6efb88a0c8524173f8a174
PriyamAryan21 <155085476+PriyamAryan21@users.noreply.github.com>
2026-02-02 19:55:06 +0530

    Fix merge conflict in toggleMenu function
    Removed merge conflict markers from toggleMenu function.

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
+
 function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-=======
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
->>>>>>> 0b3833d (Initial commit)
 }
